ff135aea1cc9ef58d873a3556b0bf1de0a4971bb
User <leonardo.1701625066@ucaldas.edu.co>
2022-10-07 20:39:16 -0500

    add Embera Language

--- Assets/Writing/script/ReadInput.cs
+++ Assets/Writing/script/ReadInput.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReadInput : MonoBehaviour
+public class ReadInput: MonoBehaviour
 {
     public string response;
 
